28b5da789e43ce7d1d4826a33e2a346d12d9be08
Evani <evanilach@gmail.com>
2023-04-11 23:20:48 +0200

    added cuda

--- n_antigens.sh
+++ n_antigens.sh
@@ -1,5 +1,5 @@
-python3 -u main.py --n_antigens 1
-python3 -u main.py --n_antigens 2
-python3 -u main.py --n_antigens 3
-python3 -u main.py --n_antigens 4
-python3 -u main.py --n_antigens 5
+python3 -u main.py --n_antigens 1 --dev cuda:0
+python3 -u main.py --n_antigens 2 --dev cuda:0
+python3 -u main.py --n_antigens 3 --dev cuda:0
+python3 -u main.py --n_antigens 4 --dev cuda:0
+python3 -u main.py --n_antigens 5 --dev cuda:0
