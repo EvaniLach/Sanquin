19f813d0a2c11302088181daab80c96c37bdca0f
E.T. Lachmansingh <s1949624@duranium.liacs.nl>
2023-05-04 14:31:46 +0200

    changed dqn.py

--- dqn.py
+++ dqn.py
@@ -7,7 +7,6 @@ import gym
 import random
 
 from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
 
 
 from tensorflow.keras.layers import Conv2D, BatchNormalization, Dense, Input
