677cfa9302eb3b6a80e2df1851fbbc61dc52d186
Evani <evanilach@gmail.com>
2023-07-12 14:17:47 +0200

    increase n trials

--- kickstart_optuna.py
+++ kickstart_optuna.py
@@ -125,7 +125,7 @@ def objective(trial):
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="minimize")
-    study.optimize(objective, n_trials=50, timeout=None)
+    study.optimize(objective, n_trials=100, timeout=None)
 
     pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
     complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
