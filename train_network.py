080349e8d5055da2c8fce5be1f3ab2662055777e
E.T. Lachmansingh <s1949624@duranium.liacs.nl>
2023-09-20 17:10:30 +0200

    tests

--- train_network.py
+++ train_network.py
@@ -72,12 +72,12 @@ def validate(model, val_loader, device):
 def multi_acc(y_pred, y_test):
     y_pred_softmax = torch.log_softmax(y_pred, dim=1)
     _, y_pred_tags = torch.max(y_pred_softmax, dim=1)
-
+    print(y_pred_tags)
     correct_pred = (y_pred_tags == y_test).float()
     acc = correct_pred.sum() / len(correct_pred)
-
+    
     acc = torch.round(acc * 100)
-
+    print(acc)
     return acc
 
 
