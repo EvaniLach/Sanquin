cf5d2482d1d284433de7de1076e1393f2206372c
Evani <evanilach@gmail.com>
2023-09-22 11:16:13 +0200

    changed seed and lr

--- main_network.py
+++ main_network.py
@@ -21,9 +21,9 @@ parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
 parser.add_argument('--epochs', type=int, default=500, metavar='N',
                     help='number of epochs to train (default: 100)')
-parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
+parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
                     help='learning rate (default: 0.001)')
-parser.add_argument('--seed', type=int, default=10, metavar='S',
+parser.add_argument('--seed', type=int, default=99, metavar='S',
                     help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=1000, metavar='N',
                     help='how many batches to wait before logging training status')
