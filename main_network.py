6446a2df5d09aab7a0ba4b9e1193ad7836fe34eb
Evani <evanilach@gmail.com>
2023-09-29 15:44:58 +0200

    print device

--- main_network.py
+++ main_network.py
@@ -206,6 +206,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     device = ('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
 
     torch.manual_seed(args.seed)
     # mp.set_start_method('spawn', force=True)
