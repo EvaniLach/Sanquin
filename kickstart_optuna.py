a37033bed39a7ac3890ebbeb8061d0770598e708
Evani <evanilach@gmail.com>
2023-09-29 12:39:18 +0200

    print GPU allocated

--- kickstart_optuna.py
+++ kickstart_optuna.py
@@ -205,11 +205,12 @@ def objective(trial):
 
 
 if __name__ == "__main__":
-    study = optuna.create_study(direction="minimize")
 
     max_memory_allocated = torch.cuda.max_memory_allocated()
 
     print(max_memory_allocated)
+
+    study = optuna.create_study(direction="minimize")
 
     study.optimize(objective, n_trials=100, timeout=None)
 
